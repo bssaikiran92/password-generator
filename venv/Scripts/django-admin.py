58357a9d64ae8c44ac2956d5c3da6b8f4acb9dc9
saibojan <bssaikiran92@gmail.com>
2020-05-25 16:42:03 -0700

    pwd_gen first commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\bssai\Desktop\Sai\python\django_proj\pwd_gen\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
